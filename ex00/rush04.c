6e541fae3f48cae13119fa623d7a597b86c67021
Seyed Mostafa Hashemian <shashemi@c2r19s3.42madrid.com>
2024-08-10 18:30:52 +0200

    a

---
+++ ex00/rush04.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shashemi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 14:45:55 by shashemi          #+#    #+#             */
+/*   Updated: 2024/08/10 17:47:16 by shashemi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	if (y < 1 || x < 1)
+		write(1, "Oye, solo se aceptan dimensiones positivas", 42);
+	while (i <= y)
+	{
+		j = 1;
+		while (j <= x)
+		{
+			if ((i == 1 && j == 1) || (i == y && j == x))
+				ft_putchar('A');
+			else if ((i == 1 && j == x) || (i == y && j == 1))
+				ft_putchar('C');
+			else if ((i == 1 || i == y) || (j == 1 || j == x))
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+}
